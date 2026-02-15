811f27811dd95fe0f5807718a4638d2b72e5573c
eth-tran <ethan.tran704@gmail.com>
2026-02-15 17:05:53 -0500

    Q1b

--- A1Q2.R
+++ A1Q2.R
@@ -3,8 +3,8 @@ data(Animals)
 
 # a.
 
-ln_body <- log(Animals$body)
-ln_brain <- log(Animals$brain)
+Animals$ln_body <- log(Animals$body)
+Animals$ln_brain <- log(Animals$brain)
 
 plot(ln_body, ln_brain)
 
@@ -19,3 +19,32 @@ for (q in 1:6) {
 }
 
 legend("topleft", legend = paste("Degree", 1:6), col = cols, lwd = 2)
+
+
+# b.
+
+test_index <- c(2, 7, 11, 16, 26)
+train <- Animals[-test_index, ]
+test <- Animals[test_index, ]
+
+APSE <- numeric(6)
+
+for (q in 1:6) {
+  model <- lm(ln_brain ~ poly(ln_body, q), data = train)
+  y_pred <- predict(model, newdata = test)
+  print(y_pred)
+  APSE[q] <- mean((test$ln_brain - y_pred)^2)
+}
+
+results <- data.frame(
+  degree = 1:6,
+  APSE = APSE
+)
+
+results
+
+which.min(APSE) # Model 3 has the lowest APSE
+
+
+
+
